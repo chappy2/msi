8d5296c4215ed42f735adf3f613cb3f18af05655
chappy2 <Sabrina-Sachs87@web.de>
2017-04-01 23:24:30 +0200

    initial commit

---
+++ presentation/app.js
@@ -0,0 +1,20 @@
+"use strict";
+const express = require('express');
+const path = require('path');
+const revealRunInTerminal = require('../reveal-run-in-terminal/index.js');
+var config = require('config');
+let app = express();
+
+app.use(revealRunInTerminal({
+  publicPath: __dirname,
+  commandRegex: /node/,
+  log: true,
+  allowRemote: true
+}));
+
+let revealRunInTerminalPlugin = path.resolve(__dirname, '../reveal-run-in-terminal/static');
+let revealJsPath = path.resolve(__dirname, 'static/reveal.js');
+app.use(express.static(revealRunInTerminalPlugin));
+app.use(express.static(revealJsPath));
+
+app.listen(config.Server.Port,config.Server.Ip);
